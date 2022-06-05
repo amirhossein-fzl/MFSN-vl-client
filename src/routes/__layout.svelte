5abbc9df3ff69baecef3c2bdc59405d730938526
Amirhossein Fazli <amirhossein95b@gmail.com>
2022-06-05 18:19:13 +0430

    NProgress add when navigating in app

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
+    import { navigating } from '$app/stores';
+    import NProgress from 'nprogress';
     import '../styles/global.scss';
+    import '../styles/nprogress.scss';
+
+    // @ts-ignore
+    NProgress.configure({ rtl: true, parent: 'body' });
+
+    $: {
+        if ($navigating) {
+            NProgress.start();
+        }
+        if(!$navigating) {
+            NProgress.done();
+        }
+    }
 </script>
 
 <slot />
